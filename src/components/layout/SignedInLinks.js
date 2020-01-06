6896773f8b04acc4680bd2a985cfbec8a6e4d5fe
HAL_9000 <jessebadgley@HAL-9000.local>
2020-01-06 14:42:47 -0600

    linked routes on navbar

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,14 +5,14 @@ const SignedInLinks = () => {
 	return (
 		<ul className='right'>
 			<li>
-				<NavLink to='/'>New Project</NavLink>
+				<NavLink to='/create'>New Project</NavLink>
 			</li>
 			<li>
 				<NavLink to='/'>Logout</NavLink>
 			</li>
 			<li>
 				<NavLink to='/' className='btn btn-floating pink lighten-1'>
-					NN
+					JB
 				</NavLink>
 			</li>
 		</ul>
