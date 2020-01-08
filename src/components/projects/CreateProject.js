67965dce731e63346cdfb446352a9ac34a120d10
JesseBadgley <jessebadgley@gmail.com>
2020-01-07 18:43:45 -0600

    added redirect and route guarding

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createProject } from '../../redux/actions/projectActions';
+import { Redirect } from 'react-router-dom';
 
 class CreateProject extends Component {
 	state = {
@@ -16,25 +17,28 @@ class CreateProject extends Component {
 		e.preventDefault();
 		// console.log(this.state);
 		this.props.createProject(this.state);
+		this.props.history.push('/');
 	};
 	render() {
+		const { auth } = this.props;
+		if (!auth.uid) return <Redirect to='/signin' />;
 		return (
 			<div className='container'>
 				<form className='white' onSubmit={this.handleSubmit}>
-					<h5 className='grey-text text-darken-3'>Create Project</h5>
+					<h5 className='grey-text text-darken-3'>Create a New Project</h5>
 					<div className='input-field'>
-						<label htmlFor='title'>Title</label>
 						<input type='text' id='title' onChange={this.handleChange} />
+						<label htmlFor='title'>Project Title</label>
 					</div>
 					<div className='input-field'>
-						<label htmlFor='content'>Project Content</label>
 						<textarea
 							id='content'
 							className='materialize-textarea'
 							onChange={this.handleChange}></textarea>
+						<label htmlFor='content'>Project Content</label>
 					</div>
 					<div className='input-field'>
-						<button className='btn pink lighten-1 z-depth-0'>Create</button>
+						<button className='btn pink lighten-1'>Create</button>
 					</div>
 				</form>
 			</div>
@@ -42,10 +46,16 @@ class CreateProject extends Component {
 	}
 }
 
+const mapStateToProps = (state) => {
+	return {
+		auth: state.firebase.auth
+	};
+};
+
 const mapDispatchToProps = (dispatch) => {
 	return {
 		createProject: (project) => dispatch(createProject(project))
 	};
 };
 
-export default connect(null, mapDispatchToProps)(CreateProject);
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject);
