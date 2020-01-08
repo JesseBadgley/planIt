f44060212de4452a133456191147e8e759107528
JesseBadgley <jessebadgley@gmail.com>
2020-01-08 17:29:33 -0600

    ordered notifications and projects desc

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,17 +8,17 @@ import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
 	render() {
-		// console.log(this.props);
-		const { projects, auth } = this.props;
+		const { projects, auth, notifications } = this.props;
 		if (!auth.uid) return <Redirect to='/signin' />;
+
 		return (
-			<div className='dashboard container '>
-				<div className='row '>
+			<div className='dashboard container'>
+				<div className='row'>
 					<div className='col s12 m6'>
 						<ProjectList projects={projects} />
 					</div>
 					<div className='col s12 m5 offset-m1'>
-						<Notifications />
+						<Notifications notifications={notifications} />
 					</div>
 				</div>
 			</div>
@@ -27,14 +27,18 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-	console.log(state);
+	// console.log(state);
 	return {
 		projects: state.firestore.ordered.projects,
-		auth: state.firebase.auth
+		auth: state.firebase.auth,
+		notifications: state.firestore.ordered.notifications
 	};
 };
 
 export default compose(
 	connect(mapStateToProps),
-	firestoreConnect([{ collection: 'projects' }])
+	firestoreConnect([
+		{ collection: 'projects', orderBy: ['createdAt', 'desc'] },
+		{ collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
+	])
 )(Dashboard);
